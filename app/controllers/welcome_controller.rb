c878f62fa6fcaa6c432366cb3483dbdbe3bc30ba
Vitaly Omelchenko <vitaly.omelchenko@onapp.com>
2012-06-12 10:13:41 +0300

    some fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
 
     if gallery_single_mode?
-      @galleries = User.first.blank? ? nil : User.first.galleries
+      @galleries = Gallery.all#User.first.blank? ? nil : User.first.galleries
     end
   end
 end
