66d1cef5360449d46f10db48f6cb695502332b61
Vitaly Omelchenko <vitaly.omelchenko@onapp.com>
2012-07-12 13:56:39 +0300

    add posibility delete gallery

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -12,6 +12,20 @@ class GalleriesController < ApplicationController
     @new_gallery = Gallery.new()
   end
 
+  def edit
+    @gallery  = Gallery.find_by_url_name(params[:id])
+  end
+
+  def destroy
+    @gallery  = Gallery.find_by_url_name(params[:id])
+
+
+    FileUtils.rm_rf(Rails.public_path + @gallery.directory_path)
+    @gallery.photos.destroy_all
+    @gallery.destroy
+    redirect_to root_path
+  end
+
   def create
     @new_gallery = current_user.galleries.build(params[:gallery])
     if @new_gallery.save
